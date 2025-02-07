d0a3bd8fec2db177e127620da7e947b4fdb37af4
Angela <angxlfish@gmail.com>
2025-02-07 05:08:29 +0000

    revert buffer implementation for now + use table.move

--- recipes-core/fluentbit-container/files/delay.lua
+++ recipes-core/fluentbit-container/files/delay.lua
@@ -2,11 +2,8 @@
 -- Lua script stays in memory for the lifetime of the Fluent Bit process,
 -- so all local variables persist across filter calls.
 
-local DELAY_SEC = 120 -- Delay (in seconds) before flushing logs
+local DELAY_SEC = 20 -- Delay (in seconds) before flushing logs
 local buckets = {} -- Table to bucket logs by their timestamp (seconds)
-local current_buffered_records = 0 -- Number of currently buffered logs, kept track of manually
-local MAX_BUFFERED_LOG_RECORDS = 65536 -- Max amount of log records to keep in memory before truncating
-local truncated_buckets = {} -- Tracks which timestamps were truncated (and emits that information only when the time comes)
 local earliest_sec = nil -- Tracks the earliest second we have in our buckets table
 local last_processed_second = nil -- Tracks which second we last ran the flush logic on
 
@@ -20,17 +17,11 @@ function log_delay(tag, ts_table, record)
         earliest_sec = arrival_sec
     end
 
-	-- Check if we are not exceeding the limit of records
-	if current_buffered_records < MAX_BUFFERED_LOG_RECORDS then
-		-- 1) Insert the new record into its bucket
-		if not buckets[arrival_sec] then
-			buckets[arrival_sec] = {}
-		end
-		table.insert(buckets[arrival_sec], record)
-		current_buffered_records = current_buffered_records + 1
-	else
-		truncated_buckets[arrival_sec] = true
-	end
+    -- 1) Insert the new record into its bucket
+    if not buckets[arrival_sec] then
+        buckets[arrival_sec] = {}
+    end
+    table.insert(buckets[arrival_sec], record)
 
     -- 2) Check if we've already processed this second
     if last_processed_second == now_floor then
@@ -43,31 +34,18 @@ function log_delay(tag, ts_table, record)
     local to_emit = {}
 
     -- Flush all buckets whose second <= (now_sec - DELAY_SEC)
-	local buckets_were_truncated = false
     while earliest_sec and earliest_sec <= (now_sec - DELAY_SEC) do
-		buckets_were_truncated = buckets_were_truncated || truncated_buckets[earliest_sec] 
-		table.remove(truncated_buckets, earliest_sec) 
-
         local bucket_logs = buckets[earliest_sec]
         if bucket_logs then
-            -- Move all logs in this bucket to 'to_emit'
-            for _, old_record in ipairs(bucket_logs) do
-                table.insert(to_emit, old_record)
-				current_buffered_records = current_buffered_records - 1
-            end
-			-- TODO: should probably delete right?
+            -- Use table.move to quickly merge bucket_logs into to_emit
+            -- (start index = 1, end index = #bucket_logs, insert destination = #to_emit+1)
+            table.move(bucket_logs, 1, #bucket_logs, #to_emit + 1, to_emit)
             buckets[earliest_sec] = nil
-            table.remove(buckets, earliest_sec)
         end
 
         -- Move on to the next second
         earliest_sec = earliest_sec + 1
     end
-
-	-- 3.5) Append truncation notice if any
-	if buckets_were_truncated then
-		table.insert(to_emit, {"Some entries were truncated!"})
-	end
 
     -- 4) Return any flushed logs
     if #to_emit == 0 then
